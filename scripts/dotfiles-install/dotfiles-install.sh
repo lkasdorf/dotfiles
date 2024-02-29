d5562f79463a2dcde14aeb52988af73ed70b95e7
Leon <u53r7@pm.me>
2024-02-29 18:39:05 +0300

    added dotfiles install script

---
+++ scripts/dotfiles-install/dotfiles-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git clone --bare https://gitlab.com/lkfiles/dotfiles.git $HOME/dotfiles
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .dotfiles-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
